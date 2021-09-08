04257b548496dd60a8278985d9a9f3a2325f7757
GIRARD Alexandre <alexandre.girard@maif.fr>
2021-09-08 17:08:24 +0200

    some bad code

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,10 +1,19 @@
 package com.mycompany.app;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        ArrayList list = new ArrayList();
+
+        new ArrayList();
+
+        ArrayList anotherList = new ArrayList();
     }
 }
